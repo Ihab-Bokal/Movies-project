af1c31332c0b31c36ac4185e0846863ebefffefa
Ihab Bokal <bokalihab@gmail.com>
2024-01-03 02:35:10 +0100

    Make movie filter functional and refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule, RouterOutlet} from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
 import {MovieService} from "./movie_service/movie.service";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, RouterModule, HttpClientModule],
+  imports: [CommonModule, RouterOutlet, RouterModule, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   providers: [MovieService, HttpClient]
@@ -16,4 +16,31 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 export class AppComponent {
   title = 'Movies';
+  private apiUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=e00128444fea107a774c8d460e7db626';
+  private API_TOKEN = "e00128444fea107a774c8d460e7db626";
+  private apiUrl1 = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) {}
+
+  public getMovieById(id:number): Observable<any>{
+    return this.http.get<any>('https://api.themoviedb.org/3/movie/' + id + '?api_key=' + this.API_TOKEN + '&language=fr')
+  }
+
+  getMovies() : Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+
+  getImageUrl(poster_path: string){
+    return 'https://image.tmdb.org/t/p/w500' + poster_path
+  }
+
+
+
+  // postClient(clt:User): Observable<boolean> {
+  //   return this.http.post<boolean>(`${this.apiUrl1}/user/saveUser`, clt);
+  // }
+  //
+  // getClient():Observable<User[]> {
+  //   return this.http.get<User[]>(`${this.apiUrl1}/user/getUser`);
+  // }
 }
--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { MovieService } from "../movie_service/movie.service";
 import { Movie } from "../model/movie";
 import { Observable } from 'rxjs';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 /* This component is going to define the card that'll contain all the data about each movie */
@@ -20,10 +20,17 @@ import { FormsModule } from '@angular/forms';
 
 export class MovieDetailsComponent implements OnInit {
   movie : Observable<Movie | undefined>;
+  private apiUrl1 = 'http://localhost:8080';
+  is_favorite: boolean= false;
 
-  constructor( private route: ActivatedRoute, public movieService: MovieService) {
+  constructor( private route: ActivatedRoute, public movieService: MovieService, private http: HttpClient) {
     const filmId = this.route.snapshot.params['id'];
     this.movie  = this.movieService.getPopularMoviesById(filmId);
+  }
+
+  public makeFavorite(): boolean {
+    console.log(true)
+    return this.is_favorite = true
   }
 
   ngOnInit() {
@@ -40,6 +47,15 @@ export class MovieDetailsComponent implements OnInit {
     if (this.newComment.trim() !== '') {
       this.comments.push(this.newComment);
       this.newComment = '';
+      // Add code to POST to backend
     }
+  }
+
+  getComment(id_film:number): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${this.apiUrl1}/Commentaire/commentaires?id=${id_film}`);
+  }
+
+  postComment(comment: Comment): Observable<Comment> {
+    return this.http.post<Comment>(`${this.apiUrl1}/comment/postComment`, comment);
   }
 }
